33516c3e8ab480ea3907dddf4efb79b686e7cb5f
Abenezer Baheru <101478749+Abenezer-Baheru@users.noreply.github.com>
2022-09-21 19:50:24 +0300

    Update 121-avl_insert.c

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,109 +1,123 @@
 #include "binary_trees.h"
 
 /**
- * check_balance - checks the balance of each node
- *
- * @node: pointer to the node
+ * balance_left - balances left side
+ * @node: pointer to temp node
+ * Return: root of tree
+ */
+avl_t *balance_left(avl_t *node)
+{
+	avl_t *z, *x, *y, *ret;
+
+	z = node;
+	y = z->left;
+	if (!y)
+		return (NULL);
+	x = binary_tree_balance(y) < 0 ? y->right : y->left;
+	if (!x)
+		return (NULL);
+	if (x == y->left)
+	{
+		ret = binary_tree_rotate_right(z);
+		return (ret);
+	}
+	binary_tree_rotate_left(y);
+	ret = binary_tree_rotate_right(z);
+	return (ret);
+}
+
+/**
+ * rebalance - rebalance AVL tree
+ * @node: pointer to node to rebalance
+ * @tree: double pointer to root of tree
+ * Return: pointer to input node
+ */
+avl_t *rebalance(avl_t *node, avl_t **tree)
+{
+	avl_t *tmp, *root, *z, *x, *y, *ret = NULL;
+	int bal;
+
+	tmp = node;
+	while (tmp)
+	{
+		bal = binary_tree_balance(tmp);
+		if (bal > 1)
+		{
+			root = balance_left(tmp);
+			if (root)
+				*tree = root;
+		}
+		else if (bal < -1)
+		{
+			z = tmp;
+			y = z->right;
+			if (y)
+			{
+				x = binary_tree_balance(y) < 0 ? y->right : y->left;
+				if (x)
+				{
+					if (x == y->right)
+					{
+						ret = binary_tree_rotate_left(z);
+					} else
+					{
+						binary_tree_rotate_right(y);
+						ret = binary_tree_rotate_left(z);
+					}
+				}
+			}
+			if (ret)
+				*tree = ret;
+		}
+		tmp = tmp->parent;
+	}
+	return (node);
+}
+
+/**
+ * avl_insert - inserts a value in an AVL Tree
+ * @tree: double pointer to root of tree
  * @value: input value
- * Return: no return
- */
-void check_balance(avl_t **node, int value)
-{
-	int balance;
-
-	balance = binary_tree_balance(*node);
-
-	if (balance > 1 && value < (*node)->left->n)
-	{
-		*node = binary_tree_rotate_right(*node);
-		return;
-	}
-
-	if (balance < -1 && value > (*node)->right->n)
-	{
-		*node = binary_tree_rotate_left(*node);
-		return;
-	}
-
-	if (balance > 1 && value > (*node)->left->n)
-	{
-		(*node)->left = binary_tree_rotate_left((*node)->left);
-		*node = binary_tree_rotate_right(*node);
-		return;
-	}
-
-	if (balance < -1 && value < (*node)->right->n)
-	{
-		(*node)->right = binary_tree_rotate_right((*node)->right);
-		*node = binary_tree_rotate_left(*node);
-		return;
-	}
-}
-
-/**
- * avl_in - checks if node is inserted
- *
- * @tree: tree root
- * @value: node value
- * Return: pointer to the new node
- */
-avl_t *avl_in(avl_t **tree, int value)
-{
-	avl_t *node;
-
-	if (value < (*tree)->n)
-	{
-		if ((*tree)->left == NULL)
-		{
-			(*tree)->left = binary_tree_node(*tree, value);
-			return ((*tree)->left);
-		}
-		else
-		{
-			node = avl_in(&((*tree)->left), value);
-			if (node)
-				check_balance(tree, value);
-			return (node);
-		}
-	}
-
-	if (value > (*tree)->n)
-	{
-		if ((*tree)->right == NULL)
-		{
-			(*tree)->right = binary_tree_node(*tree, value);
-			return ((*tree)->right);
-		}
-		else
-		{
-			node = avl_in(&((*tree)->right), value);
-			if (node)
-				check_balance(tree, value);
-			return (node);
-		}
-	}
-
-	return (NULL);
-}
-
-/**
- * avl_insert - inserts a value in a AVL Tree
- *
- * @tree: tree root
- * @value: node value
- * Return: pointer to the new node
+ * Return: pointer to the created node, or NULL on failure
  */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-	avl_t *node;
+	avl_t *av, *node;
 
-	if (*tree == NULL)
+	if (!tree)
+		return (NULL);
+	av = calloc(1, sizeof(avl_t));
+	if (!av)
+		return (NULL);
+	av->n = value;
+	if (!*tree)
 	{
-		*tree = binary_tree_node(NULL, value);
-		return (*tree);
+		return (*tree = av);
 	}
-
-	node = avl_in(tree, value);
-
-	return (node);
+	node = *tree;
+	while (node)
+	{
+		if (value == node->n)
+			return (free(av), NULL);
+		if (value < node->n)
+		{
+			if (!node->left)
+			{
+				av->parent = node;
+				node->left = av;
+				return (rebalance(node->left, tree));
+			}
+			node = node->left;
+		} else
+		{
+			if (!node->right)
+			{
+				av->parent = node;
+				node->right = av;
+				return (rebalance(node->right, tree));
+			}
+			node = node->right;
+		}
+	}
+	return (free(av), NULL);
 }
